bcd00e27fe5f81fc97f776b519b8ea742e8780c9
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-06 10:07:24 +0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
-// var checkboxElement = document.getElementById("myCheckbox");
+var checkboxElement = document.getElementById("myCheckbox");
 
-// function gender() {
+function gender() {
 
-//     if (checkboxElement.checked) {
-//         // console.log("Checkbox is checked.");
-//         console.log("Value is: " + checkboxElement.value);
-//         return checkboxElement.value;
-//     } else {
-//         console.log("Checkbox is not checked.");
-//         return null;
-//     }
-// }
+    if (checkboxElement.checked) {
+        // console.log("Checkbox is checked.");
+        console.log("Value is: " + checkboxElement.value);
+        return checkboxElement.value;
+    } else {
+        console.log("Checkbox is not checked.");
+        return null;
+    }
+}
